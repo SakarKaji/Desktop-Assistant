604097d42cea0c1fb5ddb5d07444aa1df8071148
sakar <sakarkd@gmail.com>
2024-01-23 12:44:50 +0545

    speak and command

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,38 @@
 import pyttsx3
-import speech_recognition as speech_recognition
+import speech_recognition as sr
 
 
 engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voice')
-engine.setProperty('voice',voices[0].id)
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
 engine.setProperty('rate',150)
 
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source: #microphone as source
+        print("Listening...") #print listening.. which makes sure it is listening
+        r.pause_threshold = 1 #pausee threshold 1 sec so that it listens for 1 sec
+        audio = r.listen(source) 
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio ,language='en')
+        print(f"USer said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        #speak("Say that again please...")
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if 'hello' in query:
+            print("yes sir")
+            speak("yes sir mero naam jarvi")
